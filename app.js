ff5d777ccd2663464e88ffb90110f16a1722885f
The Anh <nhoxtheanh@gmail.com>
2019-11-29 21:33:22 +0700

    Add hbs store

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/product',(req,res)=>{
   res.render('product');
 });
 
+app.get('/store',(req,res)=>{
+  res.render('store');
+});
+
 const PORT=3000;
 app.listen(PORT,()=>{
   console.log(`Listing at port ${PORT}!`);

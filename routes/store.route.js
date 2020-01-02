9c8042a43cff8a7553b641e3f5cd1460e04c9abe
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-01-02 19:26:59 +0700

    Show new products in 10 minutes

--- routes/store.route.js
+++ routes/store.route.js
@@ -1,14 +1,38 @@
 const express = require("express");
 var storeModel = require("../models/store.model");
 const config = require("../config/default.json");
+var moment = require('moment');
+const db = require("./../utils/db");
 
 const router = express.Router();
 
+
+function getJsonFromUrl(url) {
+  var json = {};
+  json = JSON.parse('{"' + decodeURI(url.replace(/&/g, "\",\"").replace(/=/g,"\":\"")) + '"}')
+  return json;
+}
+
 router.get("/", async (req, res) => {
   const rows = await storeModel.all();
+  var today = moment();
+  console.log(s+ "AAAAAAAAAA");
+  for (let i=0;i<rows.length;i++)
+  {
+    var timeStart = moment(rows[i].startDate);
+    var s = today.diff(timeStart, 'seconds')
+    if (s<=600)
+    {
+      rows[i].new = true;
+    }
+  }
+  rows.reverse(); 
   res.render("store", {
     products: rows,///////////////////////////////////////////////////////////
     empty: rows.length === 0
   });
 });
+
+
+
 module.exports = router;

59900a61f29780d3cbab4486f5335585425349f3
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-01-02 19:29:46 +0700

    Search product by name , by category + save key search , check boxes in sessionStorage

---
+++ public/js/search.js
@@ -0,0 +1,41 @@
+function SendRequestClickCheckBoxs()  
+{  
+    var checkboxes = document.getElementsByName("boxes"); 
+    var urlSend = "/store/search?searchInput=";
+    var inputSearch=sessionStorage.getItem('draft');
+    urlSend+=inputSearch;
+    for(let i = 0; i < checkboxes.length; i++)  
+    {  
+        if (checkboxes[i].checked==true)
+        {
+           urlSend+="&"+ checkboxes[i].id + "=true";
+        }
+        checkboxes[i].addEventListener('change',function(){
+            if (checkboxes[i].checked==true)
+                sessionStorage.setItem(`${checkboxes[i].id}`, 'checked');
+            else
+                sessionStorage.setItem(`${checkboxes[i].id}`, 'unchecked');
+        })    
+    }
+    window.location.replace(urlSend); 
+}  
+ var urlCurrent = window.location.href;
+ var searchInput = document.getElementById('searchInput');
+ var checkboxes = document.getElementsByName("boxes");  
+
+ for (let i=0;i<checkboxes.length;i++)
+ {
+    if (sessionStorage.getItem(`${checkboxes[i].id}`)=="checked")
+        document.getElementById(`${checkboxes[i].id}`).checked = true;
+    else
+        document.getElementById(`${checkboxes[i].id}`).checked = false;
+
+ }
+
+ if (urlCurrent.indexOf("/search")!==-1)
+    searchInput.value = sessionStorage.getItem('draft');
+
+
+ searchInput.addEventListener('change', function(){
+  sessionStorage.setItem('draft', searchInput.value);
+ });
--- routes/store.route.js
+++ routes/store.route.js
@@ -33,6 +33,51 @@ router.get("/", async (req, res) => {
   });
 });
 
+router.get("/search", async(req,res)=>{
+  const searchInput = req.query.searchInput;
+  
+  const rowsCategory = await storeModel.categoryOfSearchName(searchInput);
 
+  console.log(req.query);
+  var jsonGet ={};
+  jsonGet = req.query;
+  var sql ="select * from product where"
+  var flag=0;
+  for (const key in jsonGet)
+  {
+
+    if (key==="searchInput")
+    {
+      if (Object.keys(jsonGet).length===1)
+      {
+        sql += ` name like '%${jsonGet[key]}%'`;
+      }
+      else{
+        sql += ` and name like '%${jsonGet[key]}%'`;
+      }
+    }
+    else {
+      if (flag===0)
+      {
+        sql += ` category = ${key}`;
+        flag=1;
+      }
+      else
+      {
+        sql += ` or category = ${key}`;
+      }
+    }
+  
+  }
+  
+  const rows = await storeModel.searchbyNameCategory(sql);
+  console.log(sql);
+  res.render("store",{
+    products: rows,
+    categories: rowsCategory,
+    empty:rows.length===0,
+    input:searchInput
+  });
+});
 
 module.exports = router;
